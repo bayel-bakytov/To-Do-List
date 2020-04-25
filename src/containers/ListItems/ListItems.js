8d1367d22d2899d21480dd42283223be46adbf85
Bayel <bayel.bakytovich@gmail.com>
2020-04-25 21:40:54 +0600

    Use state in component

--- src/containers/ListItems/ListItems.js
+++ src/containers/ListItems/ListItems.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import classes from 'ListItems.module.css';
+import React, { useState } from "react";
+import classes from "./ListItems.module.css";
 
-export default ()=>  {
-    return (
-        <div className={classes.ListItems}>
-            
-        </div>
-    );
+export default () => {
+  const [items, setItems] = useState({
+    "todo-1": {
+      text: "hello world",
+      completed: false,
+    },
+    "todo-2": {
+      text: "bye world",
+      completed: true,
+    },
+  });
+  const itemsOutput = Object.keys(items).map((key) => {
+    return (<li>{items[key].text}</li>);
+  });
+  return (
+    <div className={classes.ListItems}>
+      <ul>{itemsOutput}</ul>
+    </div>
+  );
 };
