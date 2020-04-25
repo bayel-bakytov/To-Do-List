a3d06efcc4c0f06e9bead59824c2ed66fcdaf29f
Bayel <bayel.bakytovich@gmail.com>
2020-04-25 17:11:42 +0600

    Create and add ListItems component

---
+++ src/containers/ListItems/ListItems.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from 'ListItems.module.css';
+
+export default ()=>  {
+    return (
+        <div className={classes.ListItems}>
+            
+        </div>
+    );
+};
