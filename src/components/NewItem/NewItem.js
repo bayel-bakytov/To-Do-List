dc9b3ec80ec9eaa68bd1ee3423ad52f6b7c71110
Bayel <bayel.bakytovich@gmail.com>
2020-04-27 00:33:00 +0600

    Add enter behavior

--- src/components/NewItem/NewItem.js
+++ src/components/NewItem/NewItem.js
@@ -5,13 +5,20 @@ import Button from "../UI/Button/Button";
 export default ({ addItem }) => {
   function addButtonClicked(event) {
     const input = event.target.previousSibling;
+
     addItem(input.value);
     input.value = "";
     input.focus();
   }
+  function inputKeyDown({ key, target }) {
+    if (key === "Enter") {
+      addItem(target.value);
+      target.value = "";
+    }
+  }
   return (
     <div className={classes.NewItem}>
-      <input type="text" />
+      <input type="text" onKeyDown={inputKeyDown} />
       <Button click={addButtonClicked} green>
         Add
       </Button>
