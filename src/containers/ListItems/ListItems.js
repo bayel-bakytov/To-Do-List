b2be70a20c779dc0d0e9c9d815dfe1b595dc6d60
Bayel <bayel.bakytovich@gmail.com>
2020-04-27 00:21:47 +0600

    add request to firebase

--- src/containers/ListItems/ListItems.js
+++ src/containers/ListItems/ListItems.js
@@ -9,7 +9,7 @@ export default () => {
   const [items, setItems] = useState(null);
 
   function deleteItem(id) {
-    axios.delete("items/" + id + ".json").then((response) => {
+    axios.delete("/items/" + id + ".json").then((response) => {
       const newItems = { ...items };
       delete newItems[id];
       setItems(newItems);
@@ -17,22 +17,27 @@ export default () => {
   }
 
   function toggleCompleteItem(id) {
-    const newItems = { ...items };
-    newItems[id].completed = !newItems[id].completed;
-    setItems(newItems);
+    axios
+      .put("/items/" + id + "/completed.json", !items[id].completed)
+      .then((response) => {
+        const newItems = { ...items };
+        newItems[id].completed = !newItems[id].completed;
+        setItems(newItems);
+      });
   }
 
   function addItem(text) {
-    const newItems = { ...items };
-    const id = `todo-$(Math.floor(Math.random() * 10000))`;
-    newItems[id] = {
-      text: text,
-      completed: false,
-    };
-    setItems(newItems);
+    axios.post("/items/.json", { text, completed: false }).then(({ data }) => {
+      const newItems = { ...items };
+      newItems[data.name] = {
+        text: text,
+        completed: false,
+      };
+      setItems(newItems);
+    });
   }
   useEffect(() => {
-    axios.get("/items.json").then((response) => setItems({ ...response.data }));
+    axios.get("/items.json").then(({ data }) => setItems({ ...data }));
   }, []);
 
   let itemsOutput = <Loading />;
